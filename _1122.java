810acd1cc480f9e9c2acb409ac22b913a5087e8b
tranduchieu-Mob <d.hieu270799@gmail.com>
2022-10-21 13:39:39 +0700

    BaiTap

--- _1122.java
+++ _1122.java
@@ -1,21 +1,21 @@
 import java.util.Arrays;
 public class _1122 {
     public static int[] relativeSortArray(int[] arr1, int[] arr2) {
-        int []farr = new int[10001];
+        int [] arr3 = new int[10001];
         for(int i=0;i<arr1.length;i++) 
-            farr[arr1[i]]++;
+            arr3[arr1[i]]++;
         
         int j=0;
         for(int i=0;i<arr2.length;i++)
-           while(farr[arr2[i]]>0){
+           while(arr3[arr2[i]]>0){
                arr1[j] = arr2[i];
-               farr[arr2[i]]--; j++;
+               arr3[arr2[i]]--; j++;
            }
         
-        for(int i=0;i<farr.length;i++)
-            while(farr[i]>0){
+        for(int i=0;i<arr3.length;i++)
+            while(arr3[i]>0){
                 arr1[j] = i;
-                farr[i]--; j++;
+                arr3[i]--; j++;
             }
 
         return arr1;
